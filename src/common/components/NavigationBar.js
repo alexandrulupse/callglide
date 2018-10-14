4da20096fe64657e8598d33d3fcc5e8642f74a5d
alex.lupse <alex.lupse@lateral-inc.com>
2018-10-14 13:34:18 +0300

    add semantic ui

--- src/common/components/NavigationBar.js
+++ src/common/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from "react";
 import RouterPropTypes from "react-router-prop-types";
+import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import { Menu, Container } from "semantic-ui-react";
 
@@ -8,10 +9,18 @@ class NavigationBar extends PureComponent {
     return (
       <Menu fixed="top" inverted>
         <Container>
-          <Menu.Item to="/dashboard"> Dashboard</Menu.Item>
-          <Menu.Item to="/user">Account</Menu.Item>
-          <Menu.Item to="/docs">Docs</Menu.Item>
-          <Menu.Item to="/support">Support</Menu.Item>
+          <Menu.Item>
+            <Link to="/dashboard"> Dashboard</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/user"> Account</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/docs"> Docs</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/support"> Support</Link>
+          </Menu.Item>
         </Container>
       </Menu>
     );
